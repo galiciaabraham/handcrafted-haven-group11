ee50ebdfc77132bdf231089a897f356cfd04aa09
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-03 20:25:22 -0600

    Initialized project and cleaned up initial set up so the team can begin working

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { oswald, montserrat } from "./ui/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Handcrafted Haven Group 11 Project',
+    default: 'Handcrafted Haven Group 11 Project'
+  },
+  description: 'This is our project version of the Handcrafted project for BYUI WDD 430.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
