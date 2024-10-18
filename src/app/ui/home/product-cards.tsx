97b3cd9924c3bfe34c98ebdab6d709ee0dd0e382
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-18 08:14:13 -0600

    Finished initial posts fetching and view. TODO: Work in the like system logic and restrains to avoid liking more than once per user.

--- src/app/ui/home/product-cards.tsx
+++ src/app/ui/home/product-cards.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image';
 import ProductPreview from '../shop/product-list';
 import { fetchAllProducts } from "@/app/utilities/data";
 
 
 export default async function NewProductWrapper() {
-// const { newProduct } = await fetchNewProducts(); <= array?
-const newProducts = await fetchAllProducts();
+const products = await fetchAllProducts();
+
+const sortedProducts = products.sort((a, b) => new Date(b.product_created_date).getTime() - new Date(a.product_created_date).getTime());
+const newProducts = sortedProducts.slice(0,3);
+
+
 return (
     <>
         <div className='flex flex-col md:flex-row items-center justify-center gap-4'>
