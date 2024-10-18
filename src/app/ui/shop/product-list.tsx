9a4856e4ac5f94a5b5ea90105cf57f6b08ebbeb7
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-18 08:14:13 -0600

    Finished initial posts fetching and view. TODO: Work in the like system logic and restrains to avoid liking more than once per user.

--- src/app/ui/shop/product-list.tsx
+++ src/app/ui/shop/product-list.tsx
@@ -15,16 +15,16 @@ export default function ProductPreview ({
             {products?.map((product) => (
                <div key={product.product_id} className="flex flex-col justify-center items-center bg-secondary-2 shadow-lg rounded-md p-4 ">
                <div className="flex justify-center items-center w-full">
-                 <Image
+                 {/* <Image
                    src={product.product_image_url}
                    className="rounded-md shadow-md"
                    width={200}
                    height={200}
                    alt={`${product.product_title}'s product picture`}
-                 />
+                 /> */}
                </div>
                <h2 className={`${oswald.className} text-main-2 p-3`}>{product.product_title}</h2>
-               <p className="font-body-text text-main-2 p-3">{formatCurrency(product.product_price)}</p>
+               <p className="font-body-text text-main-2 p-3">{product.product_price}</p>
                <button className="bg-main-1 text-main-2 p-2 rounded-md shadow-md md:hover:bg-main-2 md:hover:text-secondary-2">Buy</button>
              </div>
              
--- src/app/utilities/postTypes.ts
+++ src/app/utilities/postTypes.ts
@@ -3,7 +3,7 @@ interface Post {
   user_id: number;
   post_title: string;
   post_content: string;
-  post_create_at: string;
-  post_updated_at: string;
+  post_create_at: Date;
+  post_updated_at: Date;
   post_likes_count: number;
 }
