ac6dccd9391771045b15b8f96fd91c9e8550cd65
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-18 19:05:10 -0600

    Working on like button, it currently updates the client but not the server. TODO: Check https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations to implement server action separately from handleLikeChange

--- src/app/utilities/data.ts
+++ src/app/utilities/data.ts
@@ -1,4 +1,5 @@
  import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
 export async function fetchAllProducts () {
     try {
@@ -12,14 +13,32 @@ export async function fetchAllProducts () {
 }
 
 export async function fetchAllPosts () {
+    try {
+      const { rows } = await sql<Post>`SELECT * FROM posts`;
+      const posts = rows;
+      return posts;
+
+  } catch (err) {
+      console.error('Error fetching data', err);
+      throw new Error('Failed to fetch the posts');
+  }
+}
+
+export async function likePost ({post_id, likesCount} : {
+  post_id : number,
+  likesCount : number;
+
+}) {
+  console.log('The function gets called');  
   try {
-    const { rows } = await sql<Post>`SELECT * FROM posts`;
-    const posts = rows;
-    return posts;
-
-} catch (err) {
-    console.error('Error fetching data', err);
-    throw new Error('Failed to fetch the posts');
+    console.log('The query fails');
+    await sql`UPDATE posts SET post_likes_count = ${likesCount} WHERE post_id = ${post_id}`
+    console.log('The query succeeds');
+    revalidatePath('/feed');
+    console.log('the revalidation succeeds')
+  } catch (err) {
+    console.error('Error when submiting a like', err);
+    throw new Error(`Failed to update likes count on post ID: ${post_id}`)
+  }
 }
 
-}
