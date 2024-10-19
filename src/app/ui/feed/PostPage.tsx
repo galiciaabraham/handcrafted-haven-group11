c5b8de36f0307f7a838dad96fa262cc6cb1e50a4
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-19 17:33:18 -0600

    Now like button is updating the data in the database and survives refreshes!

--- src/app/ui/feed/PostPage.tsx
+++ src/app/ui/feed/PostPage.tsx
@@ -1,16 +1,11 @@
 
-
-import { useState } from "react";
 import PostPreview from "@/app/ui/feed/post-list";
-import { likePost } from "@/app/utilities/data";
 
 export default function PostPage({posts}:
     {
         posts: Post[];
     }
 ) {
-
-
     return (
         <main className="grid grid-cols-1 md:grid-cols-2">
         <PostPreview posts={posts} />
