788802777aad34f6c8609692ed011903bc72b2bc
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-19 11:49:25 -0600

    Still working with like button

--- src/app/ui/feed/PostPage.tsx
+++ src/app/ui/feed/PostPage.tsx
@@ -1,4 +1,4 @@
-'use client';
+
 
 import { useState } from "react";
 import PostPreview from "@/app/ui/feed/post-list";
@@ -9,24 +9,11 @@ export default function PostPage({posts}:
         posts: Post[];
     }
 ) {
-    const [renderedPosts, setRenderedPosts] = useState(posts);
-
-    const handleLikeChange = (post_id: number) => {
-        const updatedPosts = renderedPosts.map(post => {
-            if (post.post_id === post_id) {
-                const newLikesCount = post.post_likes_count + 1;
-                likePost({ post_id, likesCount: newLikesCount });
-                return { ...post, post_likes_count: newLikesCount };
-            }
-            return post;
-        });
-        setRenderedPosts(updatedPosts);
-    }
 
 
     return (
         <main className="grid grid-cols-1 md:grid-cols-2">
-        <PostPreview posts={renderedPosts} onClickLike={handleLikeChange} />
+        <PostPreview posts={posts} />
         </main>
     )
 }
