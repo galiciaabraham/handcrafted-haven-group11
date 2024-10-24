ea0bdbbbf162b870962bca34523dafcccd5d7708
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-24 10:13:38 -0600

    Corrected lint errors that were causing deployment issues

--- src/app/ui/shop/products/star.tsx
+++ src/app/ui/shop/products/star.tsx
@@ -1,14 +1,14 @@
-import filledStar from "@/app/public/images/star.png";
-import unfilledStar from "@/app/public/images/star-unfilled.png";
+// import filledStar from "@/app/public/images/star.png";
+// import unfilledStar from "@/app/public/images/star-unfilled.png";
 
-const Star = ({filled}) => {
-    <img 
-    src={filled ? filledStar : unfilledStar} 
-    alt={filled ? "filledStar" : "unfilledStar"} 
-    className="w-6 h-6"
-     />
-}
+// const Star = ({filled}) => {
+//     <img 
+//     src={filled ? filledStar : unfilledStar} 
+//     alt={filled ? "filledStar" : "unfilledStar"} 
+//     className="w-6 h-6"
+//      />
+// }
 
 
 
-export default Star;
+// export default Star;
--- src/app/utilities/definitions.ts
+++ src/app/utilities/definitions.ts
@@ -91,8 +91,8 @@ export type User = {
     user_id: string;
     product_title: string;
     product_description: string;
-    product_price: Number;
-    product_stock_quantity: Number;
+    product_price: number;
+    product_stock_quantity: number;
     category_id: string;
     product_created_date: Date;
     product_updated_date: Date;
