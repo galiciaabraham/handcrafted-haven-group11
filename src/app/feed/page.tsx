da66216c088938c0b75034a784e9c8a1f3bbd810
galiciaabraham <galicia.abraham@hotmail.com>
2024-10-24 07:48:01 -0600

    Added Create post button that redirects the user to a future form page to create a new post, the button will be hidden if there's no session

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,13 +1,18 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import FetchPosts from "@/app/ui/feed/FetchPosts";
+import AddPostButton from "@/app/ui/feed/AddPostButton"
 
 export default function Page() {
+
     return (
         <>
           <>
     <Suspense>
     <FetchPosts />
     </Suspense>
+    <div id="add-post-button" className="relative m-5">
+    <AddPostButton/>
+    </div>
     </>  
         </>
     )
